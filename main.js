5f417b07dad54863f7c278ad4876e112b4891836
zzki93 <rlaqudrms12@gmail.com>
2022-04-30 18:47:21 +0900

    omdbapi

---
+++ main.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+function fetchMovies(){
+  axios
+    .get('https://www.omdbapi.com/?apikey=7035c60c&s=frozen')
+    .then((res) => {
+      console.log(res)
+      const h1El = document.querySelector("h1")
+      const imgEl = document.querySelector("img")
+      h1El.textContent = res.data.Search[0].Title
+      imgEl.src = res.data.Search[0].Poster
+    })
+  }
+
+  fetchMovies();
